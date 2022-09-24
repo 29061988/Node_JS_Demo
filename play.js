25e6d7dd68b84b49b0ff8a4662f65dbd9e3d0764
Manoj Khandelwal <manojkhandelwal029@gmail.com>
2022-09-24 15:51:33 +0530

    Basic java script demo

---
+++ play.js
@@ -0,0 +1,11 @@
+const name ='Manoj Khandelwal'; // For declaring constant s
+const age =29;
+const hasHobbies = true;
+
+const name ='Manoj ';
+
+function summarizeUser(u_name,u_age,u_hasHobby){
+    return ('Name is '+u_name+' Age is '+u_age+' Hobbies are there '+u_hasHobby);
+}
+
+console.log(summarizeUser(name,age,hasHobbies));
